173419b5e52a7c97bbcd286919057679af19d537
Tina-Sterite <tsterite@gmail.com>
2025-01-27 15:11:57 -0500

    reverted back to original project

--- app/main.py
+++ app/main.py
@@ -1,66 +1,80 @@
 import streamlit as st
 from streamlit.runtime.uploaded_file_manager import UploadedFile
-from typing import List
 
 from clustered_summary import ClusteredSummary
 from pdf_document import PdfDocument
 from summary import Summary
 from utils import count_tokens
 from validator import Validator
-from datetime import datetime
+from youtube_video import YouTubeVideo
+
 
 def main():
-    st.title("Path to Publishing")
+    st.title("Document Summarizer")
 
-    input_method = "Upload documents"
-
-    if input_method == "Upload documents":
-        uploaded_files = st.file_uploader(
-            "Upload documents",
-            type=["pdf"],
-            accept_multiple_files=True,
-        )
-
-    export_to_docx = st.checkbox("Export summary to a Word document")    
-
-    st.sidebar.markdown("# [Contact me by email!](mailto:tsterite@gmail.com)")
-    st.sidebar.markdown(
-        "# [Check out my other projects!](https://github.com/Tina-Sterite)"
+    input_method = st.radio(
+        "Select input method", ("Upload a document", "Enter a YouTube URL")
     )
 
-    if st.button("Generate"):
-        if input_method == "Upload documents":
-            if uploaded_files is None:
+    if input_method == "Upload a document":
+        uploaded_file = st.file_uploader(
+            "Upload a document",
+            type=["pdf"],
+        )
+
+    if input_method == "Enter a YouTube URL":
+        youtube_url = st.text_input("Enter a YouTube URL")
+
+    st.sidebar.markdown("# [Contact me by email!](mailto:ethanujohnston@gmail.com)")
+    st.sidebar.markdown(
+        "# [Check out my other projects!](https://github.com/e-johnstonn)"
+    )
+    st.sidebar.markdown("# [Twitter / X](https://x.com/ethanjdev)")
+
+    if st.button("Summarize"):
+        if input_method == "Upload a document":
+            if uploaded_file is None:
                 st.warning("Please upload a file.")
                 return
-            summarize_files(uploaded_files, export_to_docx)
+            summarize_file(uploaded_file)
 
-def summarize_files(uploaded_files: List[UploadedFile], export_to_docx: bool):
-    documents = [PdfDocument(file) for file in uploaded_files]
-    text_contents = [doc.text_content for doc in documents]
-    combined_text = "\n".join(text_contents)
-    validation_errors = Validator.validate_text(combined_text)
+        else:
+            if not youtube_url:
+                st.warning("Please enter a YouTube URL.")
+                return
+            summarize_youtube(youtube_url)
+
+
+def summarize_file(uploaded_file: UploadedFile):
+    document = PdfDocument(uploaded_file)
+    validation_errors = Validator.validate_text(document.text_content)
 
     if validation_errors:
         st.warning(f"Invalid input: {','.join(validation_errors)}")
         return
 
-    #st.markdown(run_summary(combined_text, f"documents"), unsafe_allow_html=True)
-    summary = run_summary(combined_text)
-    st.markdown(summary, unsafe_allow_html=True)
-   
-    if export_to_docx:
-        # Generate the timestamp
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        Summary(combined_text).export_to_docx(summary, f"p2p_{timestamp}.docx")
+    st.markdown(run_summary(document.text_content, f"document"), unsafe_allow_html=True)
 
-def run_summary(combined_text: str):
-    tokens = count_tokens(combined_text)
+
+def summarize_youtube(youtube_url: str):
+    video = YouTubeVideo(youtube_url)
+    transcript = video.get_transcript()
+    validation_errors = Validator.validate_text(transcript)
+
+    if validation_errors:
+        st.warning(validation_errors)
+        return
+
+    st.markdown(run_summary(transcript, "youtube video"), unsafe_allow_html=True)
+
+
+def run_summary(text: str, media_type: str):
+    tokens = count_tokens(text)
 
     if tokens > 100_000:
-        return ClusteredSummary(combined_text).get_summary()
+        return ClusteredSummary(text, media_type).get_summary()
     else:
-        return Summary(combined_text).get_summary()
+        return Summary(text, media_type).get_summary()
 
 
 if __name__ == "__main__":
